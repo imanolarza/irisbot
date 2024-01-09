ae8b7caa70255358640be220f3419d5e234c3f64
imanolarza <imanolarza@gmail.com>
2024-01-09 18:19:08 -0300

    limpieza

--- json_operations.py
+++ json_operations.py
@@ -1,20 +1,34 @@
 import json
 
-file_path = 'tema_diario.json'
+# Lugar en el que se carga la configuración del bot
+file_path = 'configuracion.json'
 
-def load_json():
-  with open(file_path, 'r', encoding='utf-8') as json_file:
-    res = json.load(json_file)
+# 
+def load_json() -> dict:
+    """
+    Cargar en formato de diccionario el json de configuración.
+    Retorna: Json en formato de diccionario python.
+    """
+    with open(file_path, 'r', encoding='utf-8') as json_file:
+        res = json.load(json_file)
 
-    return res
+        return res
 
-def update_json(col, value):
-  with open(file_path, 'r+', encoding='utf-8') as json_file:
-    res = json.load(json_file)
-    res[col] = value
+def update_json(col, value) -> dict:
+    """
+    Modificar archivo de configuración.
+    Args:
+        col: Columna a modificar.
+        value: Valor de la columna a modificar.
+    Retorna:
+        Resultado de Json modificado en formato de diccionario.
+    """
+    with open(file_path, 'r+', encoding='utf-8') as json_file:
+        res = json.load(json_file)
+        res[col] = value
 
-    json_file.seek(0)
-    json.dump(res, json_file)
-    json_file.truncate()
+        json_file.seek(0)
+        json.dump(res, json_file)
+        json_file.truncate()
 
-    return res
+        return res
