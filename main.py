b497269d0681e6f26ded2d8f10b80896000f3258
ima <macbook@192.168.1.8>
2024-01-11 17:27:11 -0300

    comandos de puntos

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(intents=intents)
+bot = commands.Bot(intents=intents, command_prefix='!')
 
 # Definición de categoría de tema semanal. Demomento solo funciona un comando para testear
 # TODO: Volverlo a implementar
 temas = app_commands.Group(name='temas', description='Tema diario')
+puntos = app_commands.Group(name='puntos', description='Comandos para puntos del staff')
 
 # Evento al iniciar bot
 @bot.event
@@ -30,6 +31,7 @@ async def on_ready():
     try:
         # Añadir a la lista de comandos la categoría de tema semanal
         bot.tree.add_command(temas)
+        bot.tree.add_command(puntos)
 
         synced = await bot.tree.sync()
 
@@ -41,6 +43,30 @@ async def on_ready():
     print(f'Loggeado como {bot.user}')
 
 # Definición de comandos
+# Puntos
+@puntos.command(name='agregar')
+@app_commands.describe(miembro='Miembro a ceder puntaje')
+@app_commands.describe(cantidad='Cantidad de puntos')
+async def agregar(interaction: discord.Interaction, miembro: discord.Member, cantidad:int):
+    embed = discord.Embed(
+        title=f"Staff {miembro} ha recibido",
+        color=0x2F3136
+    )
+    embed.set_author(
+        name="Punto staff",
+        icon_url=miembro.avatar.url
+    )
+    embed.add_field(name='Punto dado por:', value=interaction.user.name)
+    embed.add_field(name='Puntos:', value=cantidad)
+
+    embed.set_footer(
+        text=f"Requested by {interaction.user.name}",
+        icon_url=interaction.user.avatar.url
+    )
+
+    await interaction.response.send_message(embed=embed)
+
+# Temas
 @temas.command(name='carlos')
 async def carlos(interaction: discord.Interaction):
     # Retorna al usuario el mensaje de abajo
